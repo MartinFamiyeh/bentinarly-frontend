a7d85fd53f20a334fa228adcf8d41b9dccb71c23
Godfred Nai <godfrednai@gmail.com>
2025-05-30 12:56:50 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-full w-full">
+        <p className="text-6xl text-orange-400">Bentinarly Polls</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
